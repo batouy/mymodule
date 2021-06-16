0fa800902005197dfd83f98b5833b70a4154042c
batou <aig1999@163.com>
2021-06-16 16:51:15 +0800

    first commit

---
+++ src/Providers/ModuleServiceProvider.php
@@ -0,0 +1,8 @@
+<?php
+namespace Vendor\MyModule\Providers;
+
+use Konekt\Concord\BaseModuleServiceProvider;
+
+class ModuleServiceProvider extends BaseModuleServiceProvider
+{
+}
---
+++ src/resources/manifest.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+   'name'    => 'My Module',
+   'version' => '1.0.0'
+];
